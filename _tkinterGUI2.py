73c123d4354e39440740269e68a18f4e3e5728b8
unknown <Ozette@SYSTEM_OZETTE.(none)>
2012-07-11 12:29:14 +0200

    update

--- _tkinterGUI2.py
+++ _tkinterGUI2.py
@@ -1,24 +1,83 @@
+# -*- coding: cp1252 -*-
+# todo:
+# buttons koppelen
+# database implementeren
+# serial i/o implementeren
+
 import tkinter as tk
 
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# boek applicatie, bevat alle assets van de gui (knoppen, labels, etc.)
-class BookApplication(tk.Frame):
 
-    # Init constructor
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+# shall contain a catalog of all the Table objects  #
+Base = declarative_base()
+
+
+# # # # # # # # # # # # #
+# BEGIN DATABASE TABLES #
+class Boekenlijst(Base):
+    __tablename__ = 'boekenlijst1'
+
+    # Boek isbn, code, naam, versie en leerjaar 
+    ISBN = Column(Integer, primary_key=True)
+    code = Column(Integer)
+    name = Column(String(100))
+    version = Column(String(10))
+    year = (Integer)
+
+    def __init__(self, name, version, year):
+        self.name = name
+        self.version = version
+        self.year = year
+
+    def __repr__(self):
+        return "<Boekenlijst('%i', '%s', '%s', '%i')>" % (self.code, self.name, self.version, self.year)
+class Leerling(Base):
+    __tablename__ = 'leerling'
+
+    # Leerling nummer, volle naam en leerjaar
+    LLN = Column(Integer, primary_key=True)
+    fullname = Column(String(50))
+    year = Column(Integer)
+
+    # er is een default constructor, maar ok ..
+    def __init__(self, fullname, year):
+        self.fullname = fullname
+        self.year = year
+
+    def __repr__(self):
+        return "<Leerling('%s','%i')>" % (self.fullname, self.year)
+# END DATABASE TABLES #
+# # # # # # # # # # # #
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# BOEK BANK USER INTERFACE								#
+# bevat alle assets van de gui (knoppen, labels, etc.)  #
+class BookApplication(tk.Frame):
+	
+	# # # # # # # # #
+    # CONSTRUCTOR   #
     def __init__(self, master=None):
         tk.Frame.__init__(self, master, width=100, height=500)
         self.grid()
         self.createWidgets()
-
-    # Widget creation
+		
+	# # # # # # # # # # #
+    # WIDGET CREATION   #
     def createWidgets(self):
-
-        # Widget functions, they have acces to the widgets so no parameters required
+		
+		# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+        # WIDGET FUNCTIONS, they have acces to the widgets so no parameters required  #
         def search():
-            print('Gevonden resultaten voor: ' + nr_field.get())
+
+            Session = sessionmaker(bind=engine)
+            name = Session.query(Leerling).filter_by(LLN=nr_field.get())
+            print(name)
             # ZET HIERONDER NAAM A.D.H.V HET LEERLING NUMMER
             name.set(nr_field.get())
             # UNCOMMENT HIERONDER ALS JE LIEVER HEBT DAT HET VELD GECLEARD WORDT NA EEN ZOEK ACTIE
@@ -31,16 +90,15 @@ class BookApplication(tk.Frame):
 
         def returns():
             print('Boek innemen')
-            # CHECK HET HUIDIGE BOEK NAAR 'GEEL' // NODIG = ROOD
-            # check returned (clear from student profile)
+            # CHECK HET HUIDIGE BOEK NAAR 'GEEL' // NODIG = ROOD - YES // NO
 
         def extend():
-            # IK HEB GEEN IDEE WANNEER JE DIT NODIG ZOU HEBBEN ..
             print('Boek verlengen')
 
         def lost():
             # BOEK VERLOREN/SCHADE, BOEK EEN BOETE OP LEERLING NAAM
             print('Boek verloren')
+			#maak object van leerling en boek een boete die stackt en uiteindelijk prijs uitrekent
             #lost_frame = tk.Frame(root, height=10, width=20, bd=2, bg='#00ff00')
             #lost_frame.grid(row=0, column=2)
 
@@ -66,9 +124,10 @@ class BookApplication(tk.Frame):
             
         def reportBug():
             print('Fout melden')
-
-
-        # MENU BAR
+		
+		
+		# # # # # # #
+        # MENU BAR  #
         menu = tk.Menu(self)
         root.config(menu=menu)
         filemenu = tk.Menu(menu)
@@ -82,56 +141,78 @@ class BookApplication(tk.Frame):
         helpmenu.add_command(label='Versie', command=getVersion)
         helpmenu.add_command(label='Fout melden', command=reportBug)
         
+		
+        # # # # # #
+        # LABELS  #
         
-        # LABELS
-        #student number
+		# Student number. #
         self.StudentNr = tk.Label(self, text='LEERLING NR.:').grid(row=0, column=0)
-
-        #student name
+        # Student name. #
         name = tk.StringVar()
         name.set(' ')
         self.StudentNa = tk.Label(self, text='LEERLING NAAM:').grid(row=1, column=0)
         self.Name = tk.Label(self, textvariable=name, width=25).grid(row=1,column=1)
 
-        #image
+		
+        # Image. #
         imgLabel = tk.Label(self)
-        
-
-        # INPUT FIELDS
+		
+		
+		# # # # # # # # #
+        # INPUT FIELDS  #
         nr_field = tk.Entry(self, width=100)
         nr_field.grid(row=0, column=1, columnspan=10, padx=12, sticky=tk.E)
-
-        book_isbn = tk.Entry(self)  
-        
-        # BUTTONS
-        #search button
-        search_button = tk.Button(self, text='ZOEKEN', height=5, width=15, activebackground='#787878', command=search)
-        search_button.grid(row=0, column=13, padx=400, pady=2, sticky=tk.E)
-        #pickup button
-        pickup_button = tk.Button(self, text='BOEK AFGEVEN',  height=5, width=15, activebackground='#787878', command=pickup)
-        pickup_button.grid(row=2, column=13, padx=400, pady=2, sticky=tk.E)
-        #returns button
-        returns_button = tk.Button(self, text='BOEK INNEMEN', height=5, width=15, activebackground='#787878', command=returns)
-        returns_button.grid(row=3, column=13, padx=400, pady=2, sticky=tk.E)
-        #lost button
-        lost_button = tk.Button(self, text='BOEK VERLOREN', height=5, width=15, activebackground='#787878', command=lost)
-        lost_button.grid(row=4, column=13, padx=400, pady=2, sticky=tk.E)
-
-        # INNER FRAMES
+		
+		
+        #book_isbn = tk.Entry(self)  
+		# # # # # # # # #
+        # INNER FRAMES  #
         #color = '#f8f8f8'
         #status_frame = tk.Frame(self, height=500, width=900, bd=2, bg=color)
         #status_frame.grid(row=2, column=0)
+        
+		# # # # # #
+        # BUTTONS #
+        search_button = tk.Button(self, text='ZOEKEN', height=5, width=15, activebackground='#787878', command=search)
+        search_button.grid(row=0, column=13, padx=400, pady=2, sticky=tk.E)
+		
+        pickup_button = tk.Button(self, text='BOEK AFGEVEN',  height=5, width=15, activebackground='#787878', command=pickup)
+        pickup_button.grid(row=2, column=13, padx=400, pady=2, sticky=tk.E)
+		
+        returns_button = tk.Button(self, text='BOEK INNEMEN', height=5, width=15, activebackground='#787878', command=returns)
+        returns_button.grid(row=3, column=13, padx=400, pady=2, sticky=tk.E)
+		
+        lost_button = tk.Button(self, text='BOEK VERLOREN', height=5, width=15, activebackground='#787878', command=lost)
+        lost_button.grid(row=4, column=13, padx=400, pady=2, sticky=tk.E)
+# END BOEK BANK USER INTERFACE  #
+# # # # # # # # # # # # # # # # #
 
 
-# leerling table #
-class leerling():
-    
+		
+# # # # # # # #		
+# MAIN BELOW  #
 
 
-# MAIN BELOW #
+# Maak engine met database adres. #
+engine = create_engine('sqlite:///:memory:', echo=True)
+engine.execute('select 1').scalar()
+
+
+# Maakt de tables indien deze nog niet bestaan. #
+Base.metadata.create_all(engine)
+
+
+# Test leerling. #
+new_leerling = Leerling('vollenaam', 3)
+
+
+# Call user interface. #
 root = tk.Tk()
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth()-20,root.winfo_screenheight()-60))
 root.wm_title("Boeken bank - Cygnus Gymnasium Pieter Nieuwland College  ")
-
 app = BookApplication(master=root)
 app.mainloop()
+
+
+# END MAIN  #
+# # # # # # #
--- database.py
+++ database.py
@@ -5,13 +5,21 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+
+
 engine = create_engine('sqlite:///:memory:', echo=True)
 engine.execute('select 1').scalar()
+
+# sessie creeëren in button
+Session = sessionmaker(bind=engine)
+print('session bound to engine')
 
 # shall contain a catalog of all the Table objects
 Base = declarative_base()
 
-# leerling table
+
+
+## leerling table ##
 class Leerling(Base):
     __tablename__ = 'leerling'
 
@@ -28,13 +36,16 @@ class Leerling(Base):
     def __repr__(self):
         return "<Leerling('%s','%i')>" % (self.fullname, self.year)
 
-# boekenlijst table (leerjaar 1)
+
+
+## boekenlijst table (leerjaar 1) ##
 class Boekenlijst(Base):
     __tablename__ = 'boekenlijst1'
 
     # Boek isbn, naam, versie en leerjaar
     ISBN = Column(Integer, primary_key=True)
-    name = Column(String(50))
+    code = Column(Integer)
+    name = Column(String(100))
     version = Column(String(10))
     year = (Integer)
 
@@ -46,12 +57,19 @@ class Boekenlijst(Base):
     def __repr__(self):
         return "<Boekenlijst('%s', '%s', '%i')>" % (self.name, self.version, self.year)
 
+
 # maakt de tables indien deze nog niet bestaan 
 Base.metadata.create_all(engine) 
 
-new_leerling = Leerling('vollenaam', 3)
 
-Session = sessionmaker(bind=engine)
-print('session bound to engine')
+# test leerling
+new_leerling = Leerling('vollenaam', 3)
+#Session.add(new_leerling)
+#Session.commit(new_leerling)
+
+#open session
+#search button query for student nr
+#return fullname column
+
 
 
