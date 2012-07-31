275c778a16f73e4c00b68d0dbff6fae509c81cf4
Martin van Harmelen <Martin@vanharmelen.com>
2012-07-31 17:14:49 +0200

    Whoops, correcte spelling error

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Foreignkey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
