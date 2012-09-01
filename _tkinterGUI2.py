1c8bd4171c86a0386e931c59ef2acc7287fdd1d4
Martin van Harmelen <Martin@vanharmelen.com>
2012-09-01 11:20:40 +0200

    It works!!
    Except for the big book list or so, but it works!!
    
    Oh and ps. I haven't tried Mysql yet.

--- _tkinterGUI2.py
+++ _tkinterGUI2.py
@@ -4,12 +4,15 @@
 
 import tkinter as tk
 from database import *
+from sqlalchemy.orm.exc import NoResultFound
 
+DEBUG = True
+if DEBUG:
+    create_debug_data()
 
-money = 0
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# BOEK BANK USER INTERFACE				#
+# BOEK BANK USER INTERFACE				                #
 # bevat alle assets van de gui (knoppen, labels, etc.)  #
 class BookApplication(tk.Frame):
 
@@ -20,125 +23,165 @@ class BookApplication(tk.Frame):
 
     def create_widgets(self):
 
-        # nr_field bevat het leerling nummer
-        def search():
-            money_var.set(' ')
-            #Session = sessionmaker(bind=engine)
-            #llname = Session.query().filter_by(LLN=nr_field.get())
-            #print(llname)
-            #name.set(llname)
-            #nr_field.delete(0, tk.END)
+        def find_student():
+            self.session.commit()
+            try:
+                self.student = self.session.query(Student).filter(
+                    Student.id==self.nr_field.get()
+                ).one()
+                self.name.set(self.student.get_full_name())
+                self.nr_field.delete(0, tk.END)
+            except NoResultFound:
+                self.name.set('Deze leerling bestaat niet.')
 
-        def pickup():
-            print('Boek afgeven')
-            book = book_pickup_field.get()
-            print(book)
-            # CHECK HET HUIDIGE BOEK NAAR 'GROEN'
-            # check picked up (add to student profile)
-            book_pickup_field.delete(0, tk.END)
+        def lend_book():
+            self.session.commit()
+            if self.student is not None:
+                self.student = self.session.query(Student).filter(Student.id==self.student.id).one()
+                barcode = self.book_field.get()
+                try:
+                    self.session, book = self.student.borrow_book(self.session, barcode, self.damage_var)
+                    self.book_var.set(book.name + ' is succesvol uitgeleend.')
+                    self.book_field.delete(0, tk.END)
 
-        def returns():
-            print('Boek innemen')
-            returned_book = book_return_field.get()
-            book_return_field.delete(0, tk.END)
-            # CHECK HET HUIDIGE BOEK NAAR YES // NO
+                except NoResultFound:
+                    self.book_var.set('Dit boek bestaat niet.')
 
-        def extend():
-            print('Boek verlengen')
+                except BadReferenceError as e:
+                    self.book_var.set(str(e)[:-17] + ' is al uitgeleend.')
 
-        def lost():
-            global money
+        def return_book():
+            self.session.commit()
+            if self.student is not None:
+                self.student = self.session.query(Student).filter(Student.id==self.student.id).one()
+                barcode = self.book_field.get()
+                try:
+                    self.session, book, time = self.student.return_book(
+                        self.session,
+                        barcode,
+                        self.damage_var.get(),
+                        self.unusable_var.get()
+                    )
+                    self.book_var.set(book.name + ' is succesvol ingeleverd.')
+                    self.book_field.delete(0, tk.END)
+                    self.money_var.set(self.student.fine.__round__(2))
+                    self.time_var.set('Dit boek was ' + str(time.days) + ' dagen uitgeleend.')
 
-            lost_book = book_return_field.get()
-            book_return_field.delete(0, tk.END)
 
-            print('verloren boek: ' + lost_book)
-            money += 5
-            money_var.set('€' + str(money))
+                except NoResultFound:
+                    self.book_var.set('Dit boek bestaat niet.')
+                except BadReferenceError as e:
+                    self.book_var.set(str(e)[:-33] + ' is niet van ' + self.student.get_full_name())
+
+        def declare_lost():
+            self.session.commit()
+            if self.student is not None:
+                self.student = self.session.query(Student).filter(Student.id==self.student.id).one()
+                self.session = self.student.declare_lost(self.session)
+                self.money_var.set(self.student.fine.__round__(2))
 
         def undo():
-            # undo last action
-            # if added to student profile, clear it
-            # if removed from student profile, add it
-            # if booked a 'boete', minus it
-            # if booked a lost book ok, set it false
-            # etc.
-            print('Laatste actie ongedaan gemaakt')
+            self.session.rollback()
+            self.book_var.set('Je laatste handeling is ongedaan gemaakt.')
+            if self.student is not None:
+                self.money_var.set(self.student.fine.__round__(2))
 
-        def order():
-            print('Boek(en) bestellen')
-            # show order frame
-
-        def report():
-            print('Leerling boek overzicht')
-
-        def settings():
-            print('Programma instellingen (kleur)')
+        def all_books():
+            pass
 
         def get_version():
-            print('Versie 1.0')
-            #print op ext. frame
+            pass
 
-        def report_bug():
-            print('Fout melden')
-
-        def calc_money():
-            print('Het te betalen bedrag')
+        #Session
+        self.session = Session()
 
         # Menu
         menu = tk.Menu(self)
         root.config(menu=menu)
         filemenu = tk.Menu(menu)
         menu.add_cascade(label='Opties', menu=filemenu)
-        filemenu.add_command(label='Bestellen', command=order)
-        filemenu.add_command(label='Overzicht leerling', command=report)
-        filemenu.add_command(label='Instellingen', command=settings)
+        #filemenu.add_command(label='Bestellen', command=order)
+        filemenu.add_command(label='Overzicht leerling', command=all_books)
+        #filemenu.add_command(label='Instellingen', command=settings)
 
         helpmenu = tk.Menu(menu)
         menu.add_cascade(label='Help', menu=helpmenu)
         helpmenu.add_command(label='Versie', command=get_version)
-        helpmenu.add_command(label='Fout melden', command=report_bug)
+        #helpmenu.add_command(label='Fout melden', command=report_bug)
+
+        column = 2
+        padx = 0
+        sticky = tk.W
 
         # Student number. #
-        self.StudentNr = tk.Label(self, text='LEERLING NR.:').grid(row=0, column=0)
+        self.StudentNr = tk.Label(self, text='Leerling nr:').grid(row=0, column=0, sticky=tk.W)
+        self.student = None
 
         # leerling naam label
-        name = tk.StringVar()
-        name.set(' ')
-        self.StudentNa = tk.Label(self, text='LEERLING NAAM:').grid(row=1, column=0)
-        self.Name = tk.Label(self, textvariable=name, width=25).grid(row=1,column=1)
-
-        # te betalen bedrag
-        money_var = tk.StringVar()
-        money_var.set(' ')
-        money_label = tk.Label(self, text='TE BETALEN BERAG:').grid(row=4, column=0, sticky=tk.E)
-        money_var_label = tk.Label(self, textvariable=money_var, width=5).grid(row=4,column=1, sticky=tk.W)
-
+        self.name = tk.StringVar()
+        self.StudentNa = tk.Label(self, text='Leerling naam:')
+        self.StudentNa.grid(row=1, column=0, sticky=tk.W)
+        self.Name = tk.Label(self, textvariable=self.name, width=25)
+        self.Name.grid(row=1,column=1, sticky=tk.W)
 
         # Image
         imgLabel = tk.Label(self)
 
 
         # Input Fields
-        nr_field = tk.Entry(self, width=100)
-        nr_field.grid(row=0, column=1, columnspan=10, padx=12, sticky = tk.E)
-        book_pickup_field = tk.Entry(self, width=100)
-        book_pickup_field.grid(row=2, column=1, columnspan=10, padx=12, sticky = tk.E)
-        book_return_field = tk.Entry(self, width=100)
-        book_return_field.grid(row=3, column=1, columnspan=10, padx=12, sticky = tk.E)
+        self.nr_field = tk.Entry(self, width=100)
+        self.nr_field.grid(row=0, column=1, columnspan=10, padx=12, sticky = tk.W)
+
+        search_button = tk.Button(self, text='Zoeken', height=1, width=15, activebackground='#787878', command=find_student)
+        search_button.grid(row=0, column=column, padx=padx, pady=2, sticky=sticky)
+
+
+        self.book_field = tk.Entry(self, width=100)
+        self.book_field.grid(row=2, column=1, columnspan=10, padx=12, sticky = tk.W)
+        self.book_var = tk.StringVar()
+        self.book_label = tk.Label(self, text='Barcode:')
+        self.book_label.grid(row=2, column=0, sticky=tk.W)
+        self.book_name = tk.Label(self, textvariable=self.book_var, width=100)
+        self.book_name.grid(row=4, column=1, sticky=tk.W)
+
+        lend_button = tk.Button(self, text='Boek afgeven',  height=1, width=15, activebackground='#787878', command=lend_book)
+        lend_button.grid(row=1, column=column, padx=padx, pady=2, sticky=sticky)
+
+        self.damage_label = tk.Label(self, text='Is het boek beschadigd?')
+        self.damage_label.grid(row=3, column=0, sticky=tk.W)
+        self.damage_var = tk.IntVar()
+        self.damage_box_yes = tk.Radiobutton(self, text='Ja, het boek is beschadigd.', variable=self.damage_var,
+                                             value=1)
+        self.damage_box_yes.grid(row=3, column=1, sticky=tk.W)
+        self.damage_box_no = tk.Radiobutton(self, text='Nee, het boek is niet beschadigd.', variable=self.damage_var,
+                                             value=0)
+        self.damage_box_no.grid(row=3, column=1)
+
+        self.unusable_var = tk.IntVar()
+        self.unusable_box = tk.Checkbutton(self, text='Dit boek is onbruikbaar.', var=self.unusable_var)
+        self.unusable_box.grid(row=4, column=1, sticky=tk.W)
+
+        returns_button = tk.Button(self, text='Boek innemen', height=1, width=15, activebackground='#787878', command=return_book)
+        returns_button.grid(row=2, column=column, padx=padx, pady=2, sticky=sticky)
+
+        lost_button = tk.Button(self, text='Boek verloren', height=1, width=15, activebackground='#787878', command=declare_lost)
+        lost_button.grid(row=3, column=column, padx=padx, pady=2, sticky=sticky)
+
+        # te betalen bedrag
+
+        self.money_var = tk.IntVar()
+        self.money_label = tk.Label(self, text='Te betalen bedrag:').grid(row=5, column=0, sticky=tk.E)
+        self.money_var_label = tk.Label(self, textvariable=self.money_var, width=5).grid(row=5,column=1, sticky=tk.W)
+
+        self.time_var = tk.StringVar()
+        self.time_label = tk.Label(self, textvariable=self.time_var, width=33)
+        self.time_label.grid(row=5, column=1)
 
         # Buttons
-        search_button = tk.Button(self, text='ZOEKEN', height=5, width=15, activebackground='#787878', command=search)
-        search_button.grid(row=0, column=13, padx=400, pady=2, sticky=tk.E)
 
-        pickup_button = tk.Button(self, text='BOEK AFGEVEN',  height=5, width=15, activebackground='#787878', command=pickup)
-        pickup_button.grid(row=2, column=13, padx=400, pady=2, sticky=tk.E)
 
-        returns_button = tk.Button(self, text='BOEK INNEMEN', height=5, width=15, activebackground='#787878', command=returns)
-        returns_button.grid(row=3, column=13, padx=400, pady=2, sticky=tk.E)
-
-        lost_button = tk.Button(self, text='BOEK VERLOREN', height=5, width=15, activebackground='#787878', command=lost)
-        lost_button.grid(row=4, column=13, padx=400, pady=2, sticky=tk.E)
+        undo_button = tk.Button(self, text='Undo', height=1, width=15, activebackground='#787878', command=undo)
+        undo_button.grid(row=4, column=column, padx=padx, pady=2, sticky=sticky)
 
 
 # Main #
--- database.py
+++ database.py
@@ -2,7 +2,11 @@ import datetime
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Date
 from sqlalchemy import Boolean, Float
 from sqlalchemy.orm import sessionmaker, relationship, backref
+from sqlalchemy.exc import NoReferenceError as NRE
 from sqlalchemy.ext.declarative import declarative_base
+
+class BadReferenceError(NRE):
+    pass
 
 #from sqlalchemy.connectors import mysqldb
 #from sqlalchemy.dialects.mysql import *
@@ -31,6 +35,7 @@ class Book(Base):
     grades = Column(String(16), nullable=False)
     student_id = Column(String(32), ForeignKey('student.id'))
 
+    lent = Column(Date)
     gone = Column(Boolean, nullable=False, default=False)
 
     def get_grades(self):
@@ -43,8 +48,7 @@ class Book(Base):
             self.purchased.strftime('%d/%m-%Y')
         )
 
-    def add(barcode, ISBN, name, price, grades):
-        session = Session()
+    def add(session, barcode, ISBN, name, price, grades):
         book = Book(
             barcode=barcode,
             ISBN=ISBN,
@@ -54,7 +58,7 @@ class Book(Base):
             grades=','.join(str(g) for g in grades)
         )
         session.add(book)
-        session.commit()
+        return session
 
 
 ## student table ##
@@ -71,9 +75,9 @@ class Student(Base):
     grade = Column(Integer, nullable=False)
     group_letter = Column(String(2))
     books = relationship('Book', backref='student', order_by="Book.name")
-    fine = Column(Float)
+    fine = Column(Float, nullable=False, default=0)
 
-    def get_fullname(self):
+    def get_full_name(self):
         if self.inbetweener:
             return ' '.join((self.first_name, self.inbetweener, self.surname))
         else:
@@ -83,50 +87,60 @@ class Student(Base):
         return str(self.grade) + (self.group_letter or '')
 
     def __repr__(self):
-        return '<Student({}, {})>'.format(self.get_fullname(), self.get_group())
+        return '<Student({}, {})>'.format(self.get_full_name(), self.get_group())
 
-    def borrow_book(self, barcode):
-        session = Session()
+    def borrow_book(self, session, barcode, damaged=False):
         book = session.query(Book).filter(Book.barcode==barcode).one()
-        book.student_id = self.id
+        if book.student_id is None:
+            book.student_id = self.id
+        else:
+            raise BadReferenceError('{} is already lent.'.format(book.name))
+        book.lent = datetime.date.today()
+        book.damaged = damaged
         session.add(book)
-        session.commit()
+        return session, book
 
-    def return_book(self, barcode, damaged=False, unusable=False, fraction=1/6):
-        '''Returns a book and charges a fine if damage has increased'''
-        session = Session()
+    def return_book(self, session, barcode, damaged=False,
+                    unusable=False, fraction=1/6):
+        '''
+        Returns a book and charges a fine if damaged.
+        Returns a timedelta of the time the book was borrowed.
+        '''
         book = session.query(Book).filter(Book.barcode==barcode).one()
-        book.student_id = None
-
-        if damaged:
-            self.fine += book.price * fraction
+        if book in self.books:
+            book.student_id = None
+        else:
+            raise BadReferenceError(book.name + ' is not borrowed by this student.')
 
         if unusable:
-            self.fine += book.price * fraction
+            self.fine += book.price * fraction * 2
             book.gone = True
+        elif damaged:
+            self.fine += book.price * fraction
 
         session.add(book)
         session.add(self)
-        session.commmit()
 
-    def declare_lost(self, fraction=1/3):
+        return session, book, datetime.date.today() - book.lent
+
+    def declare_lost(self, session, fraction=1/3):
         '''
         Declares all books linked to this student as lost and charge a fine
         for them.
         '''
-        session = Session()
         for book in self.books:
             book.gone = True
+            book.student = None
             session.add(book)
             self.fine += book.price * fraction
 
         session.add(self)
-        session.commit()
+        return session
 
 # Makes the tables if they don't already exist.
 Base.metadata.create_all(engine)
 
-if DEBUG:
+def create_debug_data():
     session = Session()
     student = Student(id='abcd', first_name='Jan', surname='Klaassen', grade='4')
     session.add(student)
@@ -134,6 +148,10 @@ if DEBUG:
     book = Book(barcode='123456', ISBN=1, name='foo', price='50',
                 purchased=datetime.date.today(), grades='3,4,5')
     session.add(book)
+
+
+    student.borrow_book(session, 123456)
     session.commit()
 
-    student.borrow_book(123456)
+if DEBUG:
+    create_debug_data()
